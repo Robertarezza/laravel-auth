9eb287e559610393c26aef71ac2d4f31726b7474
Robertarezza <roberta.rezza@gmail.com>
2024-06-23 14:02:09 +0200

    feat: controller + file .env, + fylesistem.php for image

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use illuminate\Support\Str;
 use PhpParser\Node\Stmt\Return_;
@@ -46,6 +48,14 @@ class ProjectController extends Controller
         //oppure al posto di request->all(), 
         //mettiamo $request->validated() in modo da prendere solo i dati che sono stati validati
 
+        //controllo se c'è il file cover_image nel request
+        if($request->hasFile('cover_image')) {
+            //salvo il file nel strorage
+            $image_path = Storage::put('post_images', $request->cover_image);
+            // salvo il path del file nei dati da inserire nel daabase
+            $data['cover_image'] = $image_path;
+        }
+
         $project= new Project();
         $project->fill($data);
         $project->slug = Str::slug($request->title);
@@ -76,6 +86,14 @@ class ProjectController extends Controller
     {
         $data = $request->all();
         $project->slug = Str::slug($request->title);
+
+        if ($request->hasFile('cover_image')) {
+            if ($project->cover_image) {
+                Storage::delete($project->cover_image);
+            }
+            $image_path = Storage::put('post_images', $request->cover_image);
+            $data['cover_image'] = $image_path;
+        }
         $project->update($data);
         return redirect()->route('admin.projects.show', $project->slug)->with('message',  $project->title .  ' è stato modificato');
     }
@@ -85,6 +103,13 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        
+        // Se il post ha l'immagine, cancelliamola
+        if ($project->cover_image) {
+            Storage::delete($project->cover_image);
+        }
+        
+        
         $project->delete();
         return redirect()->route('admin.projects.index')->with('message', $project->title . ' è stato eliminato');
     }
